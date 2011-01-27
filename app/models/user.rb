1788c6f2194f837be37f0d17aff7bb30c918afed
mrjabba <mrjabba@gmail.com>
2011-01-26 23:24:21 -0600

    forgot users not attached to groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
 
   scope :not_in_group, lambda { |group|  
       join_clause = User.send(:sanitize_sql_array, 
-        ["LEFT OUTER JOIN memberships ON memberships.user_id = users.id WHERE memberships.group_id is not ?", group ])  
-        User.select("distinct(users.id), users.*").joins(join_clause)
+        ["LEFT OUTER JOIN memberships ON memberships.user_id = users.id WHERE memberships.group_id <> ? or memberships.group_id is null", group ])  
+        User.select("distinct(users.id), users.*").joins(join_clause) 
   }
  
     validates :username, :presence => true,
